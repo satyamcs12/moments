857792610b23a4f5bdb6290bfc41698f151ca4fa
Satyam ray <satyamray12@gmail.com>
2019-09-24 14:39:07 +0530

    refractoring code

--- src/main/java/com/moments/testdummy/Test.java
+++ src/main/java/com/moments/testdummy/Test.java
@@ -2,25 +2,25 @@ package com.moments.testdummy;
 
 public class Test {
 
-    public static void main(String[] args) {
-        Entity entity=new Entity<Integer>();
-        entity.setEntityVal("string");
-        entity.process();
+  public static void main(String[] args) {
+    Entity entity = new Entity<Integer>();
+    entity.setEntityVal("string");
+    entity.process();
+  }
+
+  static class Entity<T> {
+    T entityVal;
+
+    public T getEntityVal() {
+      return entityVal;
     }
 
-    static  class  Entity<T>{
-        T entityVal;
-
-        public T getEntityVal() {
-            return entityVal;
-        }
-
-        public void setEntityVal(T entityVal) {
-            this.entityVal = entityVal;
-        }
-
-        public void process(){
-            System.out.println(entityVal);
-        }
+    public void setEntityVal(T entityVal) {
+      this.entityVal = entityVal;
     }
+
+    public void process() {
+      System.out.println(entityVal);
+    }
+  }
 }
